92855cf7b4b1a8789cd885ec027d605be25f6e05
Lucas Román Fernandez <lukychillcontacto@gmail.com>
2025-04-16 12:54:50 -0300

    Solve Github Page problem

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { ghPages } from 'vite-plugin-gh-pages'
 
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: '/NOMBRE-DEL-REPO/' // importante para rutas
+  base: '/ProgressBar/' // importante para rutas
 })
